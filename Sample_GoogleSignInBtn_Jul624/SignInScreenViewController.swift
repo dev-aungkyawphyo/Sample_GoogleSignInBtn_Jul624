09de0f6f3d2185f4cefcbabaff2987b16503e420
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-06 04:02:41 +0630

    Update port

--- Sample_GoogleSignInBtn_Jul624/SignInScreenViewController.swift
+++ Sample_GoogleSignInBtn_Jul624/SignInScreenViewController.swift
@@ -19,6 +19,34 @@ class SignInScreenViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+// MARK: @IBAction
+
+extension SignInScreenViewController {
+    @IBAction func didTappedSignInBtnAction(_ sender: UIButton) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { GIDSignInResult, error in
+            self.signinBtn.isHidden = false
+            self.signoutBtn.isHidden = true
+            self.descriptionLabel.text = "Welcome To GoogleSignIn! To continue with GoogleSignIn please hit below button."
+            guard error == nil else { return }
+            
+            // If sign in succeeded, display the app's main content View.
+            guard let signInResult = GIDSignInResult else { return }
+            let user = signInResult.user
+            
+            let emailAddress = user.profile?.email
+            let fullName = user.profile?.name
+            let familyName = user.profile?.familyName
+            let profilePicUrl = user.profile?.imageURL(withDimension: 320)
+            
+            self.descriptionLabel.text = "Hey thre, \(fullName ?? "")!"
+            self.signinBtn.isHidden = true
+            self.signoutBtn.isHidden = false
+        }
+    }
+    
+    @IBAction func didTappedSignOutBtnAction(_ sender: UIButton) {
+        GIDSignIn.sharedInstance.signOut()
+    }
+}
